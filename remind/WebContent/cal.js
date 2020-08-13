060a11332a166156656d78093c02ac1fea871644
SongMyungHoon <smh90510@gmail.com>
2020-08-13 21:52:06 +0900

    대출 계산기 완료

--- remind/WebContent/cal.js
+++
@@ -1,4 +0,0 @@
-function button_Text(name, value){
-    document.getElementById(name).value = value;
-}
-
--- remind/WebContent/index.js
+++ remind/WebContent/index.js
@@ -3,126 +3,188 @@ function inputValueChange(name, value) {
     document.getElementById(name).value = value;
 }
 
+function loanAddOnClick(value) {
+    let loanVal = document.getElementById('loanInputBox_data_input').value;
+    
+    let beforeValue = loanVal == '' ? 0 : parseInt(loanVal);
+    document.getElementById('loanInputBox_data_input').value = beforeValue + parseInt(value);
+    document.getElementById('loanInputBox_data_print').innerHTML = numberWithCommas(beforeValue + parseInt(value)) + ' 원';
+}
+
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function result_cal() {
-    let outputBox_first_label_array = ['대출금', '대출금리', '대출기간', '거치기간', '상환방법', '총 이자'];
+function validation() {
     // 대출금
-    let loan_value = document.getElementById('loanInputBox_data_input').value;
+    let loanVal = parseInt(document.getElementById('loanInputBox_data_input').value);
     // 대출금리
-    let loanRate = document.getElementById('loanRateInputBox_data_input').value;
+    let loanRate = parseInt(document.getElementById('loanRateInputBox_data_input').value);
     // 대출기간
-    let loanPeriod = document.getElementById('loanPeriodInputBox_data_input').value;
+    let loanPeriod = parseInt(document.getElementById('loanPeriodInputBox_data_input').value);
     // 거치기간
-    let unredeemedTerm = document.getElementById('unredeemedTermInputBox_data_input').value;
-
-    let outputBox_first_value_array
-        = [numberWithCommas(loan_value) + '원'
-            , '연 ' + loanRate + ' %'
-            , loanPeriod + ' 개월 (' + loanPeriod/12 + ' 년)'
-            , unredeemedTerm == 0 ? '없음' : unredeemedTerm + " 개월"
-            , '원리금균등반환'
-            , numberWithCommas(Math.floor(loan_value * loanRate/100 * loanPeriod/12)) + ' 원'];
-    
-    document.write("<!DOCTYPE html>"
-                 + "<html>"
-                 + "<meta charset=\"UTF-8\">"
-                 + "<body>"
-                 + "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\"></link>"
-                 + "<link href=\"test.css\" type=\"text/css\" rel=\"stylesheet\" />"
-                 + "<link href=\"https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap\" rel=\"stylesheet\"></link>"
-                 + "<div class=\"row\">"
-                 + "    <div class=\"boxMargin-left\"></div>"
-                 + "    <div id=\"outputBox\" class=\"div-block-col-head\"><br>");
-    
-    for(i = 0; i < outputBox_first_label_array.length; i++) {
-        document.write("        <div class=\"row cell-margin text-center\">"
-                     + "            <div id=\"col-head\" class=\"col_height border border-secondary\">" + outputBox_first_label_array[i] + "</div>"
-                     + "            <div id=\"col-body\" class=\"col_height border border-secondary\">" + outputBox_first_value_array[i] + "</div>"
-                     + "        </div>");
+    let unredeemedTerm = parseInt(document.getElementById('unredeemedTermInputBox_data_input').value);
+    console.log(unredeemedTerm);
+    if (loanVal <= 0 || loanVal == NaN) {
+        alert("대출금을 바르게 입력하세요");
+        document.getElementById('loanInputBox_data_input').focus();
+        return true;
+    } else if (loanRate <= 0 || loanRate == NaN) {
+        alert("대출금리를 바르게 입력하세요");
+        document.getElementById('loanRateInputBox_data_input').focus();
+        return true;
+    } else if (loanPeriod <= 0 || loanPeriod == NaN) {
+        alert("대출기간을 바르게 입력하세요");
+        document.getElementById('loanPeriodInputBox_data_input').focus();
+        return true;
+    } else if (unredeemedTerm < 0 || unredeemedTerm == NaN) {
+        alert("거치기간을 바르게 입력하세요");
+        document.getElementById('unredeemedTermInputBox_data_input').focus();
+        return true;
     }
-    document.write("        <br>"
-                 + "        <div class=\"cell-margin text-right\">"
-                 + "            <input type=\"button\" class=\"btn btn-danger\" value=\"다시 계산하기\" onclick=\"location.href='index.html'\">"
-                 + "        </div>"
-                 + "        <br>"
-                 + "        <div class=\"row cell-margin\">"
-                 + "            <div id=\"col-num\" class=\"head_color col_height col_text_center border border-secondary\">No</div>"
-                 + "            <div id=\"col-size1\" class=\"head_color col_height col_text_center border border-secondary\">상환금</div>"
-                 + "            <div id=\"col-size1\" class=\"head_color col_height col_text_center border border-secondary\">납입원금</div>"
-                 + "            <div id=\"col-size1\" class=\"head_color col_height col_text_center border border-secondary\">이자</div>"
-                 + "            <div id=\"col-size2\" class=\"head_color col_height col_text_center border border-secondary\">납입원금계</div>"
-                 + "            <div id=\"col-size2\" class=\"head_color col_height col_text_center border border-secondary\">잔금</div>"
-                 + "        </div>");
-                 
-    finance_cal(loan_value, loanRate, loanPeriod, unredeemedTerm);
+    return false;
+}
 
-    document.write("        <br>"
-                 + "        <div class=\"cell-margin text-right\">"
-                 + "            <input type=\"button\" class=\"btn btn-danger\" value=\"다시 계산하기\" onclick=\"location.href='index.html'\">"
-                 + "        </div>"
-                 + "    <div class=\"boxMargin-right\"></div>"
-                 + "</div>"
-                 + "</body>"
-                 + "</html>");
+function resultCal() {
+    
+    // 대출금
+    let loanVal = parseInt(document.getElementById('loanInputBox_data_input').value);
+    // 대출금리
+    let loanRate = parseInt(document.getElementById('loanRateInputBox_data_input').value);
+    // 대출기간
+    let loanPeriod = parseInt(document.getElementById('loanPeriodInputBox_data_input').value);
+    // 거치기간
+    let unredeemedTerm = parseInt(document.getElementById('unredeemedTermInputBox_data_input').value);
+
+    if (validation()) {
+        return;
+    } else {
+        let inputBoxElement = document.getElementById("inputBox");
+        let outputBoxElement = document.getElementById("outputBox");
+
+        document.getElementById("output_first_tbl_loan").innerHTML = numberWithCommas(loanVal) + ' 원';
+        document.getElementById("output_first_tbl_rate").innerHTML = '연 ' + loanRate + '%';
+        console.log("loanPeriod: " + loanPeriod);
+        document.getElementById("output_first_tbl_period").innerHTML = loanPeriod + ' 개월 (' 
+                                                                    + ((loanPeriod % 12 == 0) ? (loanPeriod/12) : (loanPeriod/12).toFixed(2))
+                                                                    + ' 년)';
+        document.getElementById("output_first_tbl_unredeemedTerm").innerHTML = (unredeemedTerm == 0) ? '없음': unredeemedTerm + ' 개월';
+        
+        if(outputBoxElement.style.visibility == 'hidden' 
+        || outputBoxElement.style.visibility == '') {
+        
+            inputBoxElement.style.display = 'none';
+            outputBoxElement.style.visibility = 'visible';
+            
+            console.log("result_cal");
+            console.log("inputBoxElement display: " + inputBoxElement.style.display);
+            console.log("outputBoxElement visibility: " + outputBoxElement.style.visibility);
+
+            if(document.getElementById("monthly_result_cal").innerHTML == '') {
+                finance_cal(loanVal, loanRate, loanPeriod, unredeemedTerm);
+            }
+        }
+    }
+}
+
+function reWrite() {
+    if (confirm("입력된 내용을 삭제합니다.") == true) {
+        // 대출금
+        document.getElementById('loanInputBox_data_input').value = 0;
+        // 대출금리
+        document.getElementById('loanRateInputBox_data_input').value = 0;
+        // 대출기간
+        document.getElementById('loanPeriodInputBox_data_input').value = 0;
+        // 거치기간
+        document.getElementById('unredeemedTermInputBox_data_input').value = 0;
+
+        document.getElementById('loanInputBox_data_print').innerHTML = '0 원';
+
+        document.getElementById('loanInputBox_data_input').focus();
+        
+    } else {
+        return;
+    }
+}
+
+function retry() {
+    let inputBoxElement = document.getElementById("inputBox");
+    let outputBoxElement = document.getElementById("outputBox");
+    
+    inputBoxElement.style.display = '';
+    outputBoxElement.style.visibility = 'hidden';
+
+    console.log("retry");
+    console.log("inputBoxElement display: " + inputBoxElement.style.display);
+    console.log("outputBoxElement visibility: " + outputBoxElement.style.visibility);
+
+    document.getElementById("monthly_result_cal").innerHTML = '';
 }
 
 function finance_cal(loan, loanRate, loanPeriod, unredeemedTerm) {
+    /* loan: 대출금
+     * loanRate: 대출이자(%)
+     * loanPeriod: 상환기간 (개월)
+     * unredeemedTerm: 거치기간 (개월) */
 
-    // loan = 100,000,000
-    // loanRate = 10%
-    // loanPeriod = 
-    let principal_payment_array = [];
-    let interest_array = [];
-    let balance_array = [];
-    let accumulative_principal_payment_array = [];
+    let principalPaymentArray = [];
+    let interestArray = [];
+    let balanceArray = [];
+    let accumulativePrincipalPaymentArray = [];
     
-    // 첫 번째 달의 이자
-    let repayment_unredeemedTerm_zero = Math.floor( (loan * (loanRate/100/12) * Math.pow((1 + loanRate/100/12), loanPeriod)) / (Math.pow((1 + loanRate/100/12), loanPeriod) - 1));
+    /* 첫 번째 달의 이자
+     * 소수점 이하 버림 */
+    let repaymentUnredeemedTermZero = Math.floor( (loan * (loanRate/100/12) * Math.pow((1 + loanRate/100/12), loanPeriod)) / (Math.pow((1 + loanRate/100/12), loanPeriod) - 1));
     let interest = Math.floor(loan * loanRate/100/12);
 
     // unredeemedTerm == 0 ? : 거치기간 (unredeemedTerm) 이 0 인가
-    let repayment = parseInt(unredeemedTerm) == 0 ? repayment_unredeemedTerm_zero : interest;
-    let principal_payment = repayment - interest;
-    let balance = loan - principal_payment;
-    let accumulative_principal_payment = principal_payment;
-    let accumulative_interest = interest;
-    console.log("loanPeriod * repayment = " + numberWithCommas(loanPeriod*repayment));
-    
+    let repayment = parseInt(unredeemedTerm) == 0 ? repaymentUnredeemedTermZero : interest;
+
+    /* 거치기간에는 repayment == interest,
+     * principal_payment == 0 */
+    let principalPayment = repayment - interest;
+    let balance = loan - principalPayment;
+    let accumulativePrincipalPayment = principalPayment;
+    let accumulativeInterest = interest;
+        
     for(i = 0; i < (parseInt(unredeemedTerm) + parseInt(loanPeriod)); i++) {
         
-        interest_array.push(interest);
-        principal_payment_array.push(principal_payment);
-        balance_array.push(balance);
-        accumulative_principal_payment_array.push(accumulative_principal_payment);
-        repayment = i >= parseInt(unredeemedTerm) - 1 ? repayment_unredeemedTerm_zero : interest;
+        interestArray.push(interest);
+        principalPaymentArray.push(principalPayment);
+        balanceArray.push(balance);
+        accumulativePrincipalPaymentArray.push(accumulativePrincipalPayment);
 
-        // console.log("-------------------- No " + (i + 1) + ". --------------------");
-        // console.log("repayment : " + numberWithCommas(repayment));
-        // console.log("interest : " + numberWithCommas(interest_array[i]));
-        // console.log("principal payment : " + numberWithCommas(principal_payment_array[i]));
-        // console.log("interest + principal payment : " + numberWithCommas(interest_array[i] + principal_payment_array[i]));
-        // console.log("balance : " + numberWithCommas(balance_array[i]));
-        // console.log("accumulative principal payment : " + numberWithCommas(accumulative_principal_payment_array[i]));
-        console.log("interest: " + interest);
-        console.log("accumulative interest: " + accumulative_interest);
-        document.write("        <div class=\"row cell-margin\">"
-                        + "            <div id=\"col-num\" class=\"num_color col_height col_text_center border border-secondary\">" + (i + 1) + "</div>"
-                        + "            <div id=\"col-size1\" class=\"col_height col_text_center border border-secondary\">" + numberWithCommas(repayment) + "</div>"
-                        + "            <div id=\"col-size1\" class=\"col_height col_text_center border border-secondary\">" + numberWithCommas(principal_payment_array[i]) +"</div>"
-                        + "            <div id=\"col-size1\" class=\"col_height col_text_center border border-secondary\">" + numberWithCommas(interest_array[i]) + "</div>"
-                        + "            <div id=\"col-size2\" class=\"col_height col_text_center border border-secondary\">" + numberWithCommas(accumulative_principal_payment_array[i]) + "</div>"
-                        + "            <div id=\"col-size2\" class=\"col_height col_text_center border border-secondary\">" + numberWithCommas(balance_array[i]) + "</div>"
+        // 마지막 달 대출 잔액 처리
+        if (i == (parseInt(unredeemedTerm) + parseInt(loanPeriod)) - 1) {
+            repayment += balance;
+            principalPaymentArray[i] += balance;
+            accumulativePrincipalPaymentArray[i] += balance;
+            balanceArray[i] = 0;
+        }
+        document.getElementById("monthly_result_cal").innerHTML +=
+                         ("        <div class=\"row cell-margin\">"
+                        + "            <div class=\"col_num num_color col_height col_text_center border border-secondary\">" + (i + 1) + "</div>"
+                        + "            <div class=\"col_size1 col_height col_text_center border border-secondary\">" + numberWithCommas(repayment) + "</div>"
+                        + "            <div class=\"col_size1 col_height col_text_center border border-secondary\">" + numberWithCommas(principalPaymentArray[i]) +"</div>"
+                        + "            <div class=\"col_size1 col_height col_text_center border border-secondary\">" + numberWithCommas(interestArray[i]) + "</div>"
+                        + "            <div class=\"col_size2 col_height col_text_center border border-secondary\">" + numberWithCommas(accumulativePrincipalPaymentArray[i]) + "</div>"
+                        + "            <div class=\"col_size2 col_height col_text_center border border-secondary\">" + numberWithCommas(balanceArray[i]) + "</div>"
                         + "        </div>");
         
-        // i > unredeemedTerm : 거치기간 이 후인 경우
-        interest = i >= parseInt(unredeemedTerm) - 1 ? Math.floor(balance_array[i] * loanRate/100 / 12) : interest;
-        principal_payment = repayment - interest;
-        balance = balance_array[i] - principal_payment;
-        accumulative_principal_payment += principal_payment;
-        accumulative_interest += interest;
+        /* i > unredeemedTerm : 거치기간이 아닌 경우
+         * repayment 계산 */
+        repayment = (i >= parseInt(unredeemedTerm) - 1) ? repaymentUnredeemedTermZero : interest;
+
+        // i > unredeemedTerm : 거치기간이 아닌 경우
+        interest = (i >= parseInt(unredeemedTerm) - 1) ? Math.floor(balanceArray[i] * loanRate/100 / 12) : interest;
+        
+        principalPayment = repayment - interest;
+        balance = balanceArray[i] - principalPayment;
+        accumulativePrincipalPayment += principalPayment;
+        accumulativeInterest += interest;
+
     }
-    console.log("총 이자: " + accumulative_interest);
+    
+    // 계산된 총 이자 값을 ouput first table 의 총 이자 부분에 출력
+    document.getElementById("output_first_tbl_total_interest").innerHTML = numberWithCommas(accumulativeInterest) + " 원";
 }
